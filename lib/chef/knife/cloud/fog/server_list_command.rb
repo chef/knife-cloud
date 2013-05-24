d3ec794d4d73f07251e19672024a31b6effb5883
kaustubh-d <kaustubh@clogeny.com>
2013-05-24 18:57:27 +0530

    add functionality to handleResponse so that plugins can specify columns and formatting.

--- lib/chef/knife/cloud/fog/server_list_command.rb
+++ lib/chef/knife/cloud/fog/server_list_command.rb
@@ -20,45 +20,30 @@ class Chef
         end
 
         # When column_list is nil display all
-        # column_list_with_labels is hash with label and attribute formatting callback, ex {'key' => {label => 'label-for-key', formatter_callback => callback_method}, ...}
-        def handleResponse(servers, column_list_with_info = {})
-          # display column wise only if numbers of columns is less than 8, else as a hash for readable display.
-          display_as_columns = column_list_with_info.length.between?(1,8)
-
-          column_list = JSON.parse(servers.first.to_json).keys
-          server_list = column_list.map { |col| ui.color(column_list_with_info[col].label, :bold) } if display_as_columns
-
+        # columns_with_info is array of hash with label, key and attribute formatting callback, ex [{'label' => "Label text", key => 'key', formatter_callback => callback_method}, ...]
+        def handleResponse(servers, columns_with_info = [])
+          # display column wise only if numbers of columns is less than 8, else as a json for readable display.
           begin
+            server_list = columns_with_info.map { |col_info| ui.color(col_info[:label], :bold) } if columns_with_info.length > 0
             servers.sort_by(&:id).each do |server|
-              puts "\n{"  if not display_as_columns
-              server_json = JSON.parse(server.to_json)
-              column_list.each do |key|
-                formatter_callback = column_list_with_info[key].formatter_callback if !column_list_with_info[key].nil?
-                if key == 'state'
-                  if display_as_columns
-                    server_list << (formatter_callback.nil? ? decorate_server_state(server_json[key]) : formatter_callback.call(server_json[key]))
-                  else
-                    puts("#{ui.color(key, :bold)} = " + (formatter_callback.nil? ? decorate_server_state(server_json[key]) : formatter_callback.call(server_json[key])))
-                  end
-                else
-                  if display_as_columns
-                    server_list << (formatter_callback.nil? ? server_json[key].to_s : formatter_callback.call(server_json[key]))
-                  else
-                    puts("#{ui.color(key, :bold)} = " + (formatter_callback.nil? ? server_json[key].to_s : formatter_callback.call(server_json[key])))
-                  end
+              if columns_with_info.length > 0
+                columns_with_info.each do |col_info|
+                  server_list << (col_info[:formatter_callback].nil? ? server.send(col_info[:key]).to_s : col_info[:formatter_callback].call(server.send(col_info[:key])))
                 end
+              else
+                puts server.to_json
+                puts "\n"
               end
-              puts "\n}\n" if not display_as_columns
             end
-            rescue Excon::Errors::BadRequest => e
-              response = Chef::JSONCompat.from_json(e.response.body)
-              ui.fatal("Unknown server error (#{response['badRequest']['code']}): #{response['badRequest']['message']}")
-              raise e
-            end
-            puts ui.list(server_list, :uneven_columns_across, 8) if display_as_columns
+          rescue Excon::Errors::BadRequest => e
+            response = Chef::JSONCompat.from_json(e.response.body)
+            ui.fatal("Unknown server error (#{response['badRequest']['code']}): #{response['badRequest']['message']}")
+            raise e
+          end
+          puts ui.list(server_list, :uneven_columns_across, 8) if columns_with_info.length > 0
         end
 
-        def decorate_server_state(state)
+        def format_server_state(state)
            state = state.to_s.downcase
            case state
            when 'shutting-down','terminated','stopping','stopped','error','shutoff'
