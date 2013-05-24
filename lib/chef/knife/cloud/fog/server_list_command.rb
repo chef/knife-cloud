771b69fe9b672c598ce7d86d9b94bf3f288bb497
kaustubh-d <kaustubh@clogeny.com>
2013-05-24 13:12:35 +0530

    default to row/vertical display when more than 8 cols are listed.

--- lib/chef/knife/cloud/fog/server_list_command.rb
+++ lib/chef/knife/cloud/fog/server_list_command.rb
@@ -20,28 +20,42 @@ class Chef
         end
 
         # When column_list is nil display all
-        # column_list_with_labels is array of hash, ex [{'key' => "key-label"}, ...]
-        def handleResponse(servers, column_list_with_labels = nil)
-          column_list = JSON.parse(servers.first.to_json).keys
-          server_list = column_list.map { |col| ui.color(col, :bold) }
+        # column_list_with_labels is hash with label and attribute formatting callback, ex {'key' => {label => 'label-for-key', formatter_callback => callback_method}, ...}
+        def handleResponse(servers, column_list_with_info = {})
+          # display column wise only if numbers of columns is less than 8, else as a hash for readable display.
+          display_as_columns = column_list_with_info.length.between?(1,8)
 
-            begin
-              servers.sort_by(&:id).each do |server|
-                server_json = JSON.parse(server.to_json)
-                column_list.each do |key|
-                  if key == 'state'
-                    server_list << decorate_server_state(server_json[key])
+          column_list = JSON.parse(servers.first.to_json).keys
+          server_list = column_list.map { |col| ui.color(column_list_with_info[col].label, :bold) } if display_as_columns
+
+          begin
+            servers.sort_by(&:id).each do |server|
+              puts "\n{"  if not display_as_columns
+              server_json = JSON.parse(server.to_json)
+              column_list.each do |key|
+                formatter_callback = column_list_with_info[key].formatter_callback if !column_list_with_info[key].nil?
+                if key == 'state'
+                  if display_as_columns
+                    server_list << (formatter_callback.nil? ? decorate_server_state(server_json[key]) : formatter_callback.call(server_json[key]))
                   else
-                    server_list << server_json[key].to_s
+                    puts("#{ui.color(key, :bold)} = " + (formatter_callback.nil? ? decorate_server_state(server_json[key]) : formatter_callback.call(server_json[key])))
+                  end
+                else
+                  if display_as_columns
+                    server_list << (formatter_callback.nil? ? server_json[key].to_s : formatter_callback.call(server_json[key]))
+                  else
+                    puts("#{ui.color(key, :bold)} = " + (formatter_callback.nil? ? server_json[key].to_s : formatter_callback.call(server_json[key])))
                   end
                 end
               end
+              puts "\n}\n" if not display_as_columns
+            end
             rescue Excon::Errors::BadRequest => e
               response = Chef::JSONCompat.from_json(e.response.body)
               ui.fatal("Unknown server error (#{response['badRequest']['code']}): #{response['badRequest']['message']}")
               raise e
             end
-            puts ui.list(server_list, :uneven_columns_across, 8)
+            puts ui.list(server_list, :uneven_columns_across, 8) if display_as_columns
         end
 
         def decorate_server_state(state)
