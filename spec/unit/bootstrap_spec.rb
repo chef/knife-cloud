4309d97651cef87ba1af155897677e00e5926393
Mukta Aphale <Mukta.Aphale@Clogeny.com>
2013-06-25 01:27:23 +0530

    Bootstrap base classes, ssh & winrm protocl base classes and stub file for unit tests

---
+++ spec/unit/bootstrap_spec.rb
@@ -0,0 +1,53 @@
+# Author::
+# Copyright:: Copyright (c) 2013 Opscode, Inc.
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'chef/knife/cloud/chefbootstrap/bootstrapper'
+require 'chef/knife/cloud/chefbootstrap/bootstrap_protocol'
+require 'chef/knife/cloud/chefbootstrap/ssh_bootstrap_protocol'
+require 'chef/knife/cloud/chefbootstrap/winrm_bootstrap_protocol'
+
+describe Chef::Knife::Cloud::Bootstrapper do
+  before do
+
+  end
+
+  it "creates bootstrapper object" do
+
+  end
+
+end
+
+describe Chef::Knife::Cloud::BootstrapProtocol do
+  before do
+
+  end
+
+  it "creates BootstrapProtocol object" do
+
+  end
+
+end
+
+describe Chef::Knife::Cloud::SshBootstrapProtocol do
+  before do
+
+  end
+
+  it "creates SshBootstrapProtocol object" do
+
+  end
+
+end
+
+describe Chef::Knife::Cloud::WinrmBootstrapProtocol do
+  before do
+
+  end
+
+  it "creates WinrmBootstrapProtocol object" do
+
+  end
+
+end
+
