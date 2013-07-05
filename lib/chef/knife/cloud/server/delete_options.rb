627588ffe73ff47a712cd837184f03a9cdbc5f2c
kaustubh-d <kaustubh@clogeny.com>
2013-07-05 13:24:22 +0530

    abstract out create command cli options and reformat

--- lib/chef/knife/cloud/server/delete_options.rb
+++ lib/chef/knife/cloud/server/delete_options.rb
@@ -3,12 +3,16 @@
 # Copyright:: Copyright (c) 2013 Opscode, Inc.
 #
 
+require 'chef/knife/cloud/server/options'
+
 class Chef
   class Knife
     class Cloud
       module ServerDeleteOptions
         def self.included(includer)
           includer.class_eval do
+            include ServerOptions
+
             option :purge,
               :short => "-P",
               :long => "--purge",
@@ -16,10 +20,6 @@ class Chef
               :default => false,
               :description => "Destroy corresponding node and client on the Chef Server, in addition to destroying the Cloud node itself. Assumes node and client have the same name as the server (if not, add the '--node-name' option)."
 
-            option :chef_node_name,
-              :short => "-N NAME",
-              :long => "--node-name NAME",
-              :description => "The name of the node and client to delete, if it differs from the server name. Only has meaning when used with the '--purge' option."
           end
         end
       end
