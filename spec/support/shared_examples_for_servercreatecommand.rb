6254443dc9f7246e7f048194e469ef4c996c6cdb
Mukta A <mukta.aphale@clogeny.com>
2013-07-09 18:41:35 +0530

    Updated tests for server create command

--- spec/support/shared_examples_for_servercreatecommand.rb
+++ spec/support/shared_examples_for_servercreatecommand.rb
@@ -7,7 +7,7 @@ require 'chef/knife/cloud/server/create_command'
 
 shared_examples_for Chef::Knife::Cloud::ServerCreateCommand do |instance|
   before do
-    instance.service = mock()
+    instance.service = double
   end
 
   describe "#before_exec_command" do
@@ -15,11 +15,37 @@ shared_examples_for Chef::Knife::Cloud::ServerCreateCommand do |instance|
       instance.service.should_receive(:create_server_dependencies)
       instance.before_exec_command
     end
-    it "raises error" do
-      #instance.service.should_receive(:create_server_dependencies).and_raise(Chef::Knife::Cloud::CloudExceptions::ServerCreateDependenciesError.new("Error"))
-      #instance.service.should_receive(:delete_server_dependencies)
-      #instance.before_exec_command
+    it "delete_server_dependencies on any error" do
+      instance.service.should_receive(:create_server_dependencies).and_raise(Chef::Knife::Cloud::CloudExceptions::ServerCreateDependenciesError)
+      instance.service.should_receive(:delete_server_dependencies)
+      instance.ui.should_receive(:fatal)
+      expect {instance.before_exec_command}.to raise_error
     end
   end
+
+ describe "#execute_command" do
+
+   it "calls create_server" do
+     instance.service.should_receive(:create_server).and_return(true)
+     instance.execute_command
+   end
+   it "delete_server_dependencies on any error" do
+     instance.service.should_receive(:create_server).and_raise(Chef::Knife::Cloud::CloudExceptions::ServerCreateError)
+     instance.service.should_receive(:delete_server_dependencies)
+     instance.ui.should_receive(:fatal)
+     expect {instance.execute_command}.to raise_error
+   end
+ end
+
+ describe "#bootstrap" do
+   it "execute with correct method calls" do
+     @bootstrap = Object.new
+     @bootstrap.stub(:bootstrap)
+     Chef::Knife::Cloud::Bootstrapper.stub(:new).and_return(@bootstrap)
+     instance.should_receive(:before_bootstrap).ordered
+     instance.should_receive(:after_bootstrap).ordered      
+     instance.bootstrap
+   end
+ end  
 
 end
--- spec/unit/server_create_command_spec.rb
+++ spec/unit/server_create_command_spec.rb
@@ -7,4 +7,13 @@ require 'support/shared_examples_for_servercreatecommand'
 describe Chef::Knife::Cloud::ServerCreateCommand do
   it_behaves_like Chef::Knife::Cloud::Command, Chef::Knife::Cloud::ServerCreateCommand.new
   it_behaves_like Chef::Knife::Cloud::ServerCreateCommand, Chef::Knife::Cloud::ServerCreateCommand.new
+
+   describe "#after_exec_command" do
+   it "calls bootstrap" do
+    instance = Chef::Knife::Cloud::ServerCreateCommand.new
+    instance.should_receive(:bootstrap)
+    instance.after_exec_command
+   end
+ end
+
 end
