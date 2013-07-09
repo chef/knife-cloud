63344b1382390b1326d3ab1f1585fbee1e08052e
Mukta A <mukta.aphale@clogeny.com>
2013-07-09 16:12:50 +0530

    Adding locate_config_value correctly to bootstrap init

--- lib/chef/knife/cloud/chefbootstrap/bootstrap_protocol.rb
+++ lib/chef/knife/cloud/chefbootstrap/bootstrap_protocol.rb
@@ -28,17 +28,17 @@ class Chef
         def init_bootstrap_options
           # set the command bootstrap options.
           bootstrap.name_args = locate_config_value(:bootstrap_ip_address)
-          bootstrap.config[:chef_node_name] = locate_config_value[:chef_node_name]
-          bootstrap.config[:run_list] = locate_config_value[:run_list]
-          bootstrap.config[:prerelease] = locate_config_value[:prerelease]
-          bootstrap.config[:bootstrap_version] = locate_config_value[:bootstrap_version]
-          bootstrap.config[:distro] = locate_config_value[:distro]
-          bootstrap.config[:template_file] = locate_config_value[:template_file]
-          bootstrap.config[:bootstrap_proxy] = locate_config_value[:bootstrap_proxy]
-          bootstrap.config[:environment] = locate_config_value[:environment]
+          bootstrap.config[:chef_node_name] = locate_config_value(:chef_node_name)
+          bootstrap.config[:run_list] = locate_config_value(:run_list)
+          bootstrap.config[:prerelease] = locate_config_value(:prerelease)
+          bootstrap.config[:bootstrap_version] = locate_config_value(:bootstrap_version)
+          bootstrap.config[:distro] = locate_config_value(:distro)
+          bootstrap.config[:template_file] = locate_config_value(:template_file)
+          bootstrap.config[:bootstrap_proxy] = locate_config_value(:bootstrap_proxy)
+          bootstrap.config[:environment] = locate_config_value(:environment)
           # see chef/knife/bootstrap.rb #warn_chef_config_secret_key.
-          bootstrap.config[:encrypted_data_bag_secret] = locate_config_value[:encrypted_data_bag_secret]
-          bootstrap.config[:encrypted_data_bag_secret_file] = locate_config_value[:encrypted_data_bag_secret_file]
+          bootstrap.config[:encrypted_data_bag_secret] = locate_config_value(:encrypted_data_bag_secret)
+          bootstrap.config[:encrypted_data_bag_secret_file] = locate_config_value(:encrypted_data_bag_secret_file)
         end
 
       end
