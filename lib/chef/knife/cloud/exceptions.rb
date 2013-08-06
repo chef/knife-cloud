953ed80d60049fc210a23fe258379bddc9eddbf0
siddheshwar-more <siddheshwar.more@clogeny.com>
2013-08-06 18:40:13 +0530

    Implemented review comments to handle server cleanup on failure in after_exec_command

--- lib/chef/knife/cloud/exceptions.rb
+++ lib/chef/knife/cloud/exceptions.rb
@@ -18,7 +18,7 @@
 class Chef
   class Knife
     class Cloud
-      class CloudExceptions < Exception
+      class CloudExceptions
         class ServerCreateError < RuntimeError; end
         class ServerDeleteError < RuntimeError; end
         class ServerListingError < RuntimeError; end
