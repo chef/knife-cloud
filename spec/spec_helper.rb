e8a7a7a9dda96dcfafa803d7320085fc413daadb
prabhu-das <prabhu.das@clogeny.com>
2014-01-07 20:23:35 +0530

    Relocated TestResource class to resource_spec_helper to be reused by specific cloud spec_helpers.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,36 +1,6 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'json'
 require 'chef/knife/cloud/exceptions'
-require 'json'
 require 'chef/exceptions'
 require 'chef/config'
-
-# Creates a resource class that can dynamically add attributes to
-# instances and set the values
-module JSONModule
-  def to_json
-    hash = {}
-    self.instance_variables.each do |var|
-      hash[var] = self.instance_variable_get var
-    end
-    hash.to_json
-  end
-  def from_json! string
-    JSON.load(string).each do |var, val|
-      self.instance_variable_set var, val
-    end
-  end
-end
-
-class TestResource
-  include JSONModule
-  def initialize(*args)
-    args.each do |arg|
-      arg.each do |key, value|
-        add_attribute = "class << self; attr_accessor :#{key}; end"
-        eval(add_attribute)
-        eval("@#{key} = value")
-      end
-    end
-  end
-end
+require 'resource_spec_helper'
