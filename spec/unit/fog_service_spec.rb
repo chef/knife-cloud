8727c58938dc1d5152920f34b58a80536b30dca8
ameyavarade <ameya.varade@clogeny.com>
2014-05-29 17:04:59 +0530

    Added rspec tests for delete server

--- spec/unit/fog_service_spec.rb
+++ spec/unit/fog_service_spec.rb
@@ -55,4 +55,32 @@ describe Chef::Knife::Cloud::FogService do
       end
     end
   end
+
+  context "#delete_server" do
+    before(:each) do
+      @server = TestResource.new({:id => "test-server1"})
+      @server.define_singleton_method(:destroy){}
+    end
+
+    it "delete instance successfully" do
+      server_name = "test-server1"
+      instance.ui = double
+      instance.should_receive(:get_server).and_return(@server)
+      instance.should_receive(:get_server_name).and_return(server_name)
+      instance.should_receive(:msg_pair).with("Instance Name", server_name)
+      instance.should_receive(:msg_pair).with("Instance ID", @server.id)
+      instance.ui.should_receive(:confirm)
+      @server.should_receive(:destroy)
+      instance.delete_server(server_name)
+    end
+
+    it "raise_error on non existence server delete " do
+      server_name = "test-server1"
+      instance.ui = double
+      error_message = "Could not locate server '#{server_name}'."
+      instance.should_receive(:get_server).and_return(nil)
+      instance.ui.should_receive(:error).with(error_message)
+      expect { instance.delete_server(server_name) }.to raise_error(Chef::Knife::Cloud::CloudExceptions::ServerDeleteError, error_message)
+    end
+  end
 end
